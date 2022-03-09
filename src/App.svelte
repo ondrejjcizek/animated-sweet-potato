34baf791a56066af41e6d56470a9339bafa5864f
Ondřej Čížek <ondrej.cizek@giant.cz>
2022-03-09 15:40:32 +0100

    Two way binding, props

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,36 @@
 <script>
-	export let name;
-	export let age;
+	import ContactCard from "./ContactCard.svelte";
 
-	function incrementAge() {
-		age += 1
+	let name = "Ondrej";
+	let job = "Developer";
+	let description = "asnfunasif  nsfna  nsanfa nn janf";
+	let image = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/800px-Gull_portrait_ca_usa.jpg";
+
+	$: uppercaseName = name.toUpperCase();
+
+	function changeName() {
+		name = "Ondřej"
 	}
 </script>
 
 <style>
 	h1 {
-		color: purple;
+		color: grey;
 	}
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
-<button on:click={incrementAge}>Change Age</button>
+<h1>My name is {uppercaseName}, my age is !</h1>
+
+<button on:click={changeName}>Change Name</button>
+<input type="text" bind:value={uppercaseName}>
+<input type="text" bind:value={job}>
+<input type="text" bind:value={description}>
+<input type="text" bind:value={image}>
+<!-- <input type="text" value={name} on:input={nameInput}> -->
+
+
+<ContactCard 
+	userName={uppercaseName} 
+	jobTitle={job} 
+	{description} 
+	userImage={image} />
